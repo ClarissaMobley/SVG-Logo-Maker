b3ba4fba31b629d843617ceaadaaac4f7b74fbac
Clarissa Mobley <clarissamobley@me.com>
2024-05-07 10:45:30 -0400

    created prompt structure

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const questions = {
-    
-}
+const questions = [
+    {
+        type: "input",
+        message: "Please enter up to 3 characters for your logo",
+        name: "text",
+    },
+    {
+        type: "input",
+        message: "Please enter color keyword (OR a hexadecimal number) for your logo's text",
+        name: "textColor",
+    },
+    {
+        type: "list",
+        message: "Please select a shape for your logo",
+        choices: ["Circle", "Triangle", "Square"],
+        name: "shape",
+    },
+    {
+        type: "input",
+        message: "Please enter a color (OR a hexadecimal number) for your logo's shape.",
+        name: "shapeColor",
+    },
+];
